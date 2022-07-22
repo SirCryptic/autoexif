9bc967e927fe04b82bb574f2733095c47cb348df
sircryptic <48811414+SirCryptic@users.noreply.github.com>
2022-07-22 22:09:27 +0100

    Update autoexif.sh

--- autoexif.sh
+++ autoexif.sh
@@ -334,7 +334,6 @@ echo -e '
 read -p "└─────► " r
 echo -e '\e[0;31m───────────────────────────────────────────────────────────────────\e[0;39m'
 $r sudo bash install.sh
-$r sudo bash /usr/bin/autoexif/install.sh
 echo -e '
 ┌───────────────────────────────────────────────────────────────────┐
 │                       Fully Installed ExifTool                    │
